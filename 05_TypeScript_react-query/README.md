9630f6efb3048c3ee52946c632d2f58038d76dd9
thdud <thgml1444@naver.com>
2023-01-04 13:49:47 +0900

    #5 react-query 적용, issue생성 test

---
+++ 05_TypeScript_react-query/README.md
@@ -0,0 +1,3 @@
+### TypeScript
+
+issue test 적용할 프로젝트 생성함 #5번임 
---
+++ 05_TypeScript_react-query/src/App.tsx
@@ -0,0 +1,14 @@
+import {CounterContainer} from './containers/CounterContainer';
+import TodoApp from './containers/TodoAdd';
+
+const App = () => {
+  return (
+    <>
+      <CounterContainer />
+      <hr/>
+      <TodoApp/>
+    </>
+    )
+};
+
+export default App;
