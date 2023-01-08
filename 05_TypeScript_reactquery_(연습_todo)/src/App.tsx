2a87ddea6776674ae20d362355ddfe0347e577f7
thdud <thgml1444@naver.com>
2023-01-09 01:56:24 +0900

    typescript props를 넘겨보자

--- 05_TypeScript_reactquery_(연습_todo)/src/App.tsx
+++ 05_TypeScript_reactquery_(연습_todo)/src/App.tsx
@@ -1,12 +1,24 @@
-import Add from "./components/Add";
-import List from "./components/List";
+import { useState } from "react";
+import Todo from "./components/Todo";
+import Todolist from "./components/Todolist";
+import Prac from "./Prac";
+import PropsTest from "./PropsTest";
 
 const App = () => {
+  const [page, setPage] = useState(false);
   return (
     <>
       <h2>소영짱</h2>
-      <Add />
-      <List />
+      {/* <PropsTest testText="props를 넘겨주자" /> */}
+      <button onClick={(e) => setPage(!page)}>전환</button>
+      {page ? (
+        <>
+          <Todo />
+          <Todolist />
+        </>
+      ) : (
+        <Prac />
+      )}
     </>
   );
 };
