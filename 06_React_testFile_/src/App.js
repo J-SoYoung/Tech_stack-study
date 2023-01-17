2e179d4a44280b9c3052676e68ec3b11bcbfcbd9
thdud <thgml1444@naver.com>
2023-01-17 10:44:17 +0900

    react-window로 무한스크롤 구현 test 진행중입니다

--- 06_React_testFile_/src/App.js
+++ 06_React_testFile_/src/App.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import Modal from "./components/ModalBasic";
-import InfiniteScroll from "./infiniteScroll/InfiniteScroll";
 import KakaoShare from "./kakaoShare/KakaoShare";
+import _InfiniteScroll from "./infiniteScroll/file/_InfiniteScroll";
+import InfiniteScroll from "./infiniteScroll/file/InfiniteScroll";
+import Example from "./infiniteScroll/file/React_window";
 
 function App() {
   return (
     <>
       {/* <Modal /> */}
       <KakaoShare />
-      <InfiniteScroll />;
+      <Example />
     </>
   );
 }
