c84342c08cebf798fb008d4f15c2557f86421801
thdud <thgml1444@naver.com>
2023-03-08 16:43:29 +0900

    무한스크롤 구현 공부

--- 06_React_testFile_/src/infiniteScroll/Example1.jsx
+++ 06_React_testFile_/src/infiniteScroll/Example1.jsx
@@ -7,28 +7,29 @@ const Example1 = () => {
   const [pageNum, setPageNum] = useState(1);
   const { isLoading, isError, error, result, hasNextPage } = usePosts(pageNum);
 
-  // observer를 사용할 것임
-  const intObserver = useRef();
+  // 마지막 포스트 ref를 찾아라. useCallback으로 정의한다
+  // useCallback에 사용될 post는 우리가 갖게 될 요소 ( 마지막 ref이다 )
+  // post상태에 대한 많은 내용 추가될 것임
+  // 1. loading 상태일때
+  // 2. 등록된 관찰 대상 요소와의 관찰을 중단시키는 메서드
+  //     -> 왜 여기서 나오는지 모르겟. 언마운트때 나와야 하는거아니가, 여튼 패스
 
-  // 마지막 페이지 여부를 찾아라
-  // post => usePosts result 배열의 요소
+  // 옵저버 기본구조
+  // 콜백함수와 옵션을 받는다
+  // const io = new IntersectionObserver(callback[, options])
+  const intObserver = useRef();
   const lastPropsRef = useCallback(
     (post) => {
       if (isLoading) return;
-
-      // observer가 관찰하고 있는 곳이 있으면 중지하라
       if (intObserver.current) intObserver.current.disconnect();
 
-      // Intersection Observer API는 콜백을 생성한다
-      // 1. 대상(target)요소가 특정 요소와 교차할 때 / 2. observer가 최초로 타겟을 관측하도록 요청받을 때.
-      // isIntersecting : IntersectionObserver의 Entryr값(=posts)이 교차 관찰자의 루트와 교차하는 경우 T/F 값을 출력한다
       intObserver.current = new IntersectionObserver((posts) => {
+        console.log(posts);
         if (posts[0].isIntersecting && hasNextPage) {
           console.log("we are near the lasg post");
           setPageNum((prev) => prev + 1);
         }
       });
-      // 마지막 페이지에 속하는 post가 있으면 observer가 ref하고 있음
       if (post) intObserver.current.observe(post);
 
       // useCallback은 의존성배열을 가지고 있기 때문에 상태를 확인해야 할 필요가 있다.
@@ -42,6 +43,7 @@ const Example1 = () => {
   // 아래 렌더에서 보이는 게시물
   // 서버의 결과값을 가져와서 map돌림. 결과값이 마지막요소이면 콘솔을 찍어라
   // ExPost에 props로 데이터를 넘겨줄 것임
+
   const content = result.map((post, i) => {
     if (result.length === i + 1) {
       console.log("last ele");
@@ -58,6 +60,7 @@ const Example1 = () => {
       </h1>
       {/* 게시물 */}
       {content}
+
       {/* 게시물을 아래로 스크롤 했을 때, 빨리 결과가 나오지 않을 때를 대비하여 */}
       {isLoading && <p className="center"> Loading More Posts... </p>}
       <div className="center">
